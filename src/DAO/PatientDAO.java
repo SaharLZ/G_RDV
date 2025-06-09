4649e4b2151b123e473a90f4086c4b7bb930208d
sahar louazi <saharlouazi09@gmail.com>
2025-06-09 22:03:00 +0100

    DAO update

--- src/DAO/PatientDAO.java
+++ src/DAO/PatientDAO.java
@@ -14,13 +14,16 @@ public class PatientDAO {
 
     // CREATE
     public void ajouterPatient(Patient patient) throws SQLException {
-        String sql = "INSERT INTO Patient(Nom, Prenom, Date) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO Patient(Nom, Prenom, Date, Blood, Adress, Medical_note) VALUES (?, ?, ?, ?, ?, ?)";
         PreparedStatement ps = null;
         try {
             ps = connection.prepareStatement(sql);
             ps.setString(1, patient.getNom());
             ps.setString(2, patient.getPrenom());
             ps.setString(3, patient.getDate());
+            ps.setString(4, patient.getBlood());
+            ps.setString(5, patient.getAdress());
+            ps.setString(6, patient.getMedical_note());
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -51,6 +54,9 @@ public class PatientDAO {
                 p.setNom(rs.getString("Nom"));
                 p.setPrenom(rs.getString("Prenom"));
                 p.setDate(rs.getString("Date"));
+                p.setAdress(rs.getString("Adress"));
+                p.setBlood(rs.getString("Blood"));
+                p.setMedical_note(rs.getString("Medical_note"));
                 patients.add(p);
             }
         } catch (SQLException e) {
@@ -77,14 +83,17 @@ public class PatientDAO {
 
     // UPDATE
     public void modifierPatient(Patient patient) throws SQLException {
-        String sql = "UPDATE Patient SET Nom = ?, Prenom = ?, Date = ? WHERE Id = ?";
+        String sql = "UPDATE Patient SET Nom = ?, Prenom = ?, Date = ? , Blood = ? , Medical_note = ? WHERE Id = ?";
         PreparedStatement ps = null;
         try {
             ps = connection.prepareStatement(sql);
             ps.setString(1, patient.getNom());
             ps.setString(2, patient.getPrenom());
             ps.setString(3, patient.getDate());
-            ps.setInt(4, patient.getId());
+            ps.setString(4, patient.getBlood());
+            ps.setString(5, patient.getAdress());
+            ps.setString(6, patient.getMedical_note());
+            ps.setInt(7, patient.getId());
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
--- src/DAO/RDVDAO.java
+++ src/DAO/RDVDAO.java
@@ -3,7 +3,6 @@ package DAO;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 import Model.Ordonnance;
 import Model.Patient;
 import Model.RDV;
@@ -17,18 +16,16 @@ public class RDVDAO {
 
     // CREATE
     public void ajouterRDV(RDV rdv) throws SQLException {
-        String sql = "INSERT INTO RDV(Date, Heure, Etat,Patient, Ord) VALUES (?, ?, ?, ?,?)";
+        String sql = "INSERT INTO RDV(Date, Heure, Etat, Note, Patient, Ord) VALUES (?, ?, ?, ?, ?, ?)";
         PreparedStatement ps = null;
         try {
             ps = connection.prepareStatement(sql);
             ps.setString(1, rdv.getDate());
             ps.setString(2, rdv.getHeure());
-
-            
-            ps.setInt(3, rdv.getPatient().getId());  
-
-            
-            ps.setInt(4, rdv.getOrd().getId());  
+            ps.setString(3, rdv.getEtat());
+            ps.setString(4, rdv.getNote());
+            ps.setInt(5, rdv.getPatient().getId());
+            ps.setInt(6, rdv.getOrd().getId());
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -38,14 +35,12 @@ public class RDVDAO {
         }
     }
 
-
     // READ
     public List<RDV> getTousLesRDV() throws SQLException {
         List<RDV> liste = new ArrayList<RDV>();
         Statement stmt = null;
         ResultSet rs = null;
         String sql = "SELECT * FROM RDV";
-
         try {
             stmt = connection.createStatement();
             rs = stmt.executeQuery(sql);
@@ -54,44 +49,39 @@ public class RDVDAO {
                 r.setId(rs.getInt("Id"));
                 r.setDate(rs.getString("Date"));
                 r.setHeure(rs.getString("Heure"));
-                
-                
+                r.setEtat(rs.getString("Etat"));
+                r.setNote(rs.getString("Note"));
+
                 Patient patient = new Patient();
                 patient.setId(rs.getInt("Patient")); 
                 r.setPatient(patient);
 
-                
                 Ordonnance ordonnance = new Ordonnance();
                 ordonnance.setId(rs.getInt("Ord")); 
-                r.setOrd(ordonnance);  
+                r.setOrd(ordonnance);
 
                 liste.add(r);
             }
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             throw e;
-        } finally {
-            if (rs != null) try { rs.close(); } catch (SQLException e) { e.printStackTrace(); }
-            if (stmt != null) try { stmt.close(); } catch (SQLException e) { e.printStackTrace(); }
         }
-
         return liste;
     }
 
     // UPDATE
     public void modifierRDV(RDV rdv) throws SQLException {
-        String sql = "UPDATE RDV SET Date = ?, Heure = ?,Etat=?, Patient = ?, Ord = ? WHERE Id = ?";
+        String sql = "UPDATE RDV SET Date = ?, Heure = ?, Etat = ?, Note = ?, Patient = ?, Ord = ? WHERE Id = ?";
         PreparedStatement ps = null;
         try {
             ps = connection.prepareStatement(sql);
             ps.setString(1, rdv.getDate());
             ps.setString(2, rdv.getHeure());
-
-            ps.setInt(3, rdv.getPatient().getId());  
-
-            
-            ps.setInt(4, rdv.getOrd().getId());  
-            ps.setInt(5, rdv.getId());  
+            ps.setString(3, rdv.getEtat());
+            ps.setString(4, rdv.getNote());
+            ps.setInt(5, rdv.getPatient().getId());
+            ps.setInt(6, rdv.getOrd().getId());
+            ps.setInt(7, rdv.getId());
             ps.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -100,7 +90,6 @@ public class RDVDAO {
             if (ps != null) try { ps.close(); } catch (SQLException e) { e.printStackTrace(); }
         }
     }
-
 
     // DELETE
     public void supprimerRDV(int id) throws SQLException {
@@ -116,5 +105,6 @@ public class RDVDAO {
         } finally {
             if (ps != null) try { ps.close(); } catch (SQLException e) { e.printStackTrace(); }
         }
+    
     }
 }
